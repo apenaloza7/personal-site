e3cee07b4bd66d7c3ff927551ab922f5432257a9
apenaloza7 <apenaloza7@users.noreply.github.com>
2025-08-10 21:23:58 +0000

    Deploy to GitHub Pages 93d023b8028d6bbce2442bad45d69ea2dcae358c

--- contentful-client.js
+++ contentful-client.js
@@ -1,13 +1,17 @@
+/**
+ * Initializes the Contentful client and exposes it globally
+ * @function initializeContentfulClient
+ */
 function initializeContentfulClient() {
-    // --- Contentful Setup --- //
-    const CONTENTFUL_SPACE_ID = 'uhcip1ivkuft';
-    const CONTENTFUL_ACCESS_TOKEN = 'eRQiXFljDH30a6B4AEZRpGiQczrIJxV5LVK-o_lG7tE';
+	// --- Contentful Setup --- //
+	const CONTENTFUL_SPACE_ID = 'uhcip1ivkuft'
+	const CONTENTFUL_ACCESS_TOKEN = 'eRQiXFljDH30a6B4AEZRpGiQczrIJxV5LVK-o_lG7tE'
 
-    const client = contentful.createClient({
-        space: CONTENTFUL_SPACE_ID,
-        accessToken: CONTENTFUL_ACCESS_TOKEN,
-    });
+	const client = contentful.createClient({
+		space: CONTENTFUL_SPACE_ID,
+		accessToken: CONTENTFUL_ACCESS_TOKEN
+	})
 
-    // Expose the client to the global window object so all scripts can use it
-    window.contentfulClient = client;
+	// Expose the client to the global window object so all scripts can use it
+	window.contentfulClient = client
 }
